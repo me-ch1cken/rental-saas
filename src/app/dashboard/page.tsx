602c9a98ab57024c9a9e90bb127a49cd4bd5428d
me-ch1cken <rdecroo24@gmail.com>
2025-06-15 19:26:58 +0200

    removed placeholder dashboardpage text on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import Calendar from "@/components/blocks/calendar";
 export default function DashboardPage() {
     return (
         <>
-            {'DashboardPage'}
             <Calendar />
         </>
     )
