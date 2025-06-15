e0d8210eda4802b8a9de0eb9c51a1448c470a3d2
me-ch1cken <rdecroo24@gmail.com>
2025-06-15 22:21:59 +0200

    added breadcrumbs to dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,33 @@
 import { AppSidebar } from "@/components/blocks/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    
+
     return (
         <SidebarProvider>
             <AppSidebar />
             <main className="ms-4">
-                { children }
+                <Breadcrumb className="mt-4 mb-4">
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Overview</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+                {children}
             </main>
         </SidebarProvider>
     )
